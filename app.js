eb710ffc25b6eb495f5ed741811f7d499b2e2270
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-01-25 14:25:42 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const greet = document.querySelector('#container')
+for (let i = 1; i <= 100; i++) {
+    const btn = document.createElement('button')
+    btn.innerText = 'Hey'
+    container.appendChild(btn)
+}
